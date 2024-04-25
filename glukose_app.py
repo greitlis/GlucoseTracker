938d2bfd4899df2e805c9595b20b9a22a9658878
Meret Niklaus <niklamer@students.zhaw.ch>
2024-04-25 10:26:38 +0200

    eingabe verbessert

--- glukose_app.py
+++ glukose_app.py
@@ -22,8 +22,30 @@ def init_dataframe():
         st.session_state.df = pd.DataFrame(columns=DATA_COLUMNS)
 
 def eingabe():
-    number = st.number_input("Eingabe Blutzuckerwert", value=None, placeholder="Type a number...")
-    st.write('Dein aktueller Blutzucker ist ', number)
+    global blood_sugar
+    global measure_date
+    global measure_time
+
+    blood_sugar = st.number_input("Eingabe Blutzuckerwert", value=None, placeholder="Type a number...", min_value=0, max_value=35)
+    st.write('Dein aktueller Blutzucker ist ', blood_sugar)
+
+    measure_date = st.date_input("Eingabe Datum", datetime.now(), format="DD.MM.YYYY")
+    measure_time = st.time_input("Eingabe Uhrzeit", datetime.now())
+
+    st.write('Datum/Uhrzeit: ' + str(measure_date) + ' ' + str(measure_time))
+
+    st.button("Save", type="primary", on_click=save)
+
+def save():
+    st.write('I would now save!')
+    st.write('Wert', blood_sugar)
+    st.write('Datum/Uhrzeit:', measure_date, measure_time)
+
+    # TODO: Save to CSV
+
+blood_sugar: float
+measure_date: datetime.date
+measure_time: datetime.time
 
 
 def main():
